716c43d6cdd45411f787a7ed91d0aaaf2df88366
ElihuJones <xelijone@amazon.com>
2022-09-16 18:31:55 -0400

    Added Deck class

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -1,12 +1,14 @@
 package com.tlglearning.cards.model;
 
-public class Card {
+import java.util.Objects;
+
+public class Card implements Comparable<Card> {
 
   //Field Declarations. This is calling the enums Rank and Suit
   private final Rank rank;    //field declaration (modifier - private and final, Rank-type, rank-name
   private final Suit suit;    //field declaration (modifier-private and final, type-Suit, name-suit
 
-//Constructor created from the field declarations of rank and suit via alt+enter
+  //Constructor created from the field declarations of rank and suit via alt+enter
   public Card(Rank rank, Suit suit) {
     this.rank = rank;
     this.suit = suit;
@@ -22,13 +24,35 @@ public class Card {
   }
 
   @Override
+  public int hashCode() {
+    return Objects.hash(rank, suit);  // FIXME: Move this to initialization.
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean result;
+    if (this == obj) {
+      result = true;
+    } else if (obj instanceof Card) {
+      Card other = (Card) obj;
+      result = (this.rank == other.rank && this.suit
+          == other.suit); //you can do == with enums bc they have separate initial addresses.
+    } else {
+      result = false;
+    }
+    return result;
+  }
+
+  @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
   }
 
-  //todo: override equals method
+  @Override
+  public int compareTo(Card other) {
+    return 0; //Todo Replace with comparison based on suit and rank.
+  }
 
-  //todo: override hashcode method
 }
 
 
