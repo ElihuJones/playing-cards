d7b18bcec0e1fafa27276bf44f741d43b796226d
ElihuJones <xelijone@amazon.com>
2022-09-27 16:37:57 -0400

    ColorFirst now an inner class in Main (in-progress)

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -18,23 +18,21 @@ public class Main {
     System.out.println(deck);         //Shrink wrap order again
     deck.sort(new ColorFirst());      //
     System.out.println(deck);         // This is shrink wrap order but in descending order.
-
   }
+  private class ColorFirst implements Comparator<Card> {
 
-}
-
- class ColorFirst implements Comparator<Card> {
-
-  //This method was Overridden because the class inherits an abstract class, thus won't compile if not overridden.
-  @Override
-  public int compare(Card card1, Card card2) {
-    int comparison = card1.suit().color().compareTo(card2.suit().color());
-    if (comparison == 0) {
-      comparison = card1.suit().compareTo(card2.suit());      //use card1 to invoke the suit() method and us that to invoke compareTo card2 to invoke suit() method
+    //This method was Overridden because the class inherits an abstract class, thus won't compile if not overridden.
+    @Override
+    public int compare(Card card1, Card card2) {
+      int comparison = card1.suit().color().compareTo(card2.suit().color());
       if (comparison == 0) {
-        comparison = - card1.rank().compareTo(card2.rank());  //The -(negative) yields descending vice ascending order
+        comparison = card1.suit().compareTo(card2.suit());      //use card1 to invoke the suit() method and us that to invoke compareTo card2 to invoke suit() method
+        if (comparison == 0) {
+          comparison = - card1.rank().compareTo(card2.rank());  //The -(negative) yields descending vice ascending order
+        }
       }
+      return comparison;
     }
-    return comparison;
   }
 }
+
