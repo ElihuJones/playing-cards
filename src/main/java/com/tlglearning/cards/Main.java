edd75bdd02e7afca9e52287face6b0020ff94a55
ElihuJones <xelijone@amazon.com>
2022-09-27 17:02:36 -0400

    ColorFirst as local class of main method.

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -9,6 +9,22 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
+    class ColorFirst implements Comparator<Card> {
+
+      //This method was Overridden because the class inherits an abstract class, thus won't compile if not overridden.
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.suit().color().compareTo(card2.suit().color());
+        if (comparison == 0) {
+          comparison = card1.suit().compareTo(card2.suit());      //use card1 to invoke the suit() method and us that to invoke compareTo card2 to invoke suit() method
+          if (comparison == 0) {
+            comparison = - card1.rank().compareTo(card2.rank());  //The -(negative) yields descending vice ascending order
+          }
+        }
+        return comparison;
+      }
+    }
+
     Deck deck = new Deck();           //An instance of Deck.
     System.out.println(deck);         //String representation of the deck. Shrink wrap order
     Random rng = new SecureRandom();  //Creates an acceptable source of randomness.
@@ -20,20 +36,5 @@ public class Main {
     System.out.println(deck);         // This is shrink wrap order but in descending order.
   }
   //Now this is a static nested inner class within Main.java
-  private static class ColorFirst implements Comparator<Card> {
-
-    //This method was Overridden because the class inherits an abstract class, thus won't compile if not overridden.
-    @Override
-    public int compare(Card card1, Card card2) {
-      int comparison = card1.suit().color().compareTo(card2.suit().color());
-      if (comparison == 0) {
-        comparison = card1.suit().compareTo(card2.suit());      //use card1 to invoke the suit() method and us that to invoke compareTo card2 to invoke suit() method
-        if (comparison == 0) {
-          comparison = - card1.rank().compareTo(card2.rank());  //The -(negative) yields descending vice ascending order
-        }
-      }
-      return comparison;
-    }
-  }
 }
 
